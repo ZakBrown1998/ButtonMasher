a8743c8491c78c3b303be0b9ffd504f1c55487a3
ZakBrown1998 <43067354+ZakBrown1998@users.noreply.github.com>
2018-09-14 15:24:50 +0100

    Added Timer and Score mechanics

--- Assets/Scripts/ButtonMasher.cs
+++ Assets/Scripts/ButtonMasher.cs
@@ -6,24 +6,54 @@ public class ButtonMasher : MonoBehaviour {
 
 //Data for our class
     public AudioSource clickSound; //sound that will play when we click the button
+    public TextMesh timerText; //Displays text for the timer
+    public TextMesh scoreText; //Display text for the player's score
+    public float gameLength; //How many seconds will the game last 
+
+    private int score = 0; //the numerical data value of our score
+    private float timeRemaining = 0; //Numerical time remaining for game
+    private bool gameRunning = true;
 
     // Use this for initialization
     void Start() {
         Debug.Log("Start method called");
+
+        timeRemaining = gameLength;
+
     } //End of Start()
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Update method called");
+
+        timeRemaining = timeRemaining - Time.deltaTime;
+
+        //Update the visual time remaining
+        timerText.text = (Mathf.CeilToInt (timeRemaining)).ToString();
+
+        if (timeRemaining <= 0)
+        {
+
+            timeRemaining = 0;
+            gameRunning = false;
+        } //End of if 
     } //End of Update()
 
     
     private void OnMouseDown()
     {
         Debug.Log("OnMouseDown method called");
-        clickSound.Play();
-        //Trigger our clicking sound to play
+        //Check if the game is still running 
+        if (gameRunning == true)
+        {
+            clickSound.Play();
+            //Trigger our clicking sound to play
+            //Increase the score by 1
+            score = score + 1;
+            //Update visual score
+            scoreText.text = score.ToString();
+        } // End of if (gameRunning == true)
 
     }//End of OnMouseDown
 
